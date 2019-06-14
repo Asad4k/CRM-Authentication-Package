8e63eabc2b337c217de3284f6614030a4b75008b
Andrew Gosselin <andrew.gosselin@ntautoservices.com>
2019-06-14 09:09:18 -0400

    .

--- src/routes.php
+++ src/routes.php
@@ -3,5 +3,5 @@
 Route::group(['middleware' => ['web']], function () {
 	Route::get(config('crm_authentication.main.login_route'), 'AJG\CRM_Authentication\CRMAuthenticationController@indexLogin');
 	Route::post('/a' . config('crm_authentication.main.login_route'), 'AJG\CRM_Authentication\CRMAuthenticationController@postLogin');
-	Route::get(config('crm_authentication.main.logout_route', 'AJG\CRM_Authentication\CRMAuthenticationController@indexLogout');
+	Route::get(config('crm_authentication.main.logout_route'), 'AJG\CRM_Authentication\CRMAuthenticationController@indexLogout');
 });
